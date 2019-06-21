90b5951c923daab526a321ccccf14b063ef3986c
guihui <chenguihui3076@foxmail.com>
2019-06-22 06:59:52 +0800

    STM32F407 USB转CAN项目源码和工程文件

---
+++ Debug/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c 
+
+OBJS += \
+./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.o 
+
+C_DEPS += \
+./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.c
+	arm-atollic-eabi-gcc -c -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -std=gnu11 -D__weak=__attribute__((weak)) -D__packed=__attribute__((__packed__)) -DUSE_HAL_DRIVER -DSTM32F407xx -I../Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -g -fstack-usage -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -specs=nano.specs -o "$@" "$<"
+
---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,55 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/can.c \
+../Src/freertos.c \
+../Src/gpio.c \
+../Src/main.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_it.c \
+../Src/syscalls.c \
+../Src/system_stm32f4xx.c \
+../Src/usart.c \
+../Src/usb_device.c \
+../Src/usbd_cdc_if.c \
+../Src/usbd_conf.c \
+../Src/usbd_desc.c 
+
+OBJS += \
+./Src/can.o \
+./Src/freertos.o \
+./Src/gpio.o \
+./Src/main.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_it.o \
+./Src/syscalls.o \
+./Src/system_stm32f4xx.o \
+./Src/usart.o \
+./Src/usb_device.o \
+./Src/usbd_cdc_if.o \
+./Src/usbd_conf.o \
+./Src/usbd_desc.o 
+
+C_DEPS += \
+./Src/can.d \
+./Src/freertos.d \
+./Src/gpio.d \
+./Src/main.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_it.d \
+./Src/syscalls.d \
+./Src/system_stm32f4xx.d \
+./Src/usart.d \
+./Src/usb_device.d \
+./Src/usbd_cdc_if.d \
+./Src/usbd_conf.d \
+./Src/usbd_desc.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	arm-atollic-eabi-gcc -c -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -std=gnu11 -D__weak=__attribute__((weak)) -D__packed=__attribute__((__packed__)) -DUSE_HAL_DRIVER -DSTM32F407xx -I../Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -g -fstack-usage -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -specs=nano.specs -o "$@" "$<"
+
